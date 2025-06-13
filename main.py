33665fd4dc6471d3757100a19531962fd5ed97ec
ecastellanosr <ecastellanosrada@gmail.com>
2025-06-12 18:59:06 -0600

    added path argument in CLI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, count_character, sort
+import sys 
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,8 +7,11 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    frankenstein_fp = "./books/frankenstein.txt"
-    book_text = get_book_text(frankenstein_fp)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1] 
+    book_text = get_book_text(book_path)
     num_words = get_num_words(book_text)
     char_dict = count_character(book_text)
     sorted_list = sort(char_dict)
